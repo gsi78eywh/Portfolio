6ad589a8f1ff478f750634b168b12d65ca1ad582
gsi78eywh <your@email.com>
2026-02-25 09:46:35 +0800

    fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react";  
 import "../App.css";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGithub, FaPhp, FaJava } from "react-icons/fa";
 import { SiMysql } from "react-icons/si";
